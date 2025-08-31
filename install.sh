875066bae5ee2b26a39245bc8ab02b9dd516f841
Orwa Diraneyya <info@orwa.tech>
2025-08-31 15:52:16 +0000

    Improved the install script
    This improvement allows it us to reuse the logic
    of the installer elsewhere, simply by copying the
    code for the `create_hard_links` function

--- install.sh
+++ install.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
 
-PG_EXTENSIONS_DIR=$(pg_config --sharedir)/extension
+PG_EXTENSIONS_DIR=$(pg_config --sharedir 2>/dev/null)/extension
 
-shopt -s nullglob
-for fn in *sql *.control; do
-    ln=${fn/%.pgsql/.sql}
+create_hard_links() {
+    if [[ -z "$PG_EXTENSIONS_DIR" ]] || ! [[ -d "$PG_EXTENSIONS_DIR" ]]; then 
+        echo 'We could not locate the extension directory for the Postgres' >&2
+        return 1; 
+    fi
 
-    printf "%-24s => pg-extension-dir/%s\n%b" "$fn" "$ln" '\x1b[2m'
-    (set -x; ln -f $fn "$PG_EXTENSIONS_DIR/$ln")
-    printf "%b" '\x1b[0m'
-done
+    shopt -s nullglob
+    for fn in *sql *.control; do
+        ln=${fn/%.pgsql/.sql}
+
+        printf "%-24s => pg-extension-dir/%s\n%b" "$fn" "$ln" '\x1b[2m'
+        (set -x; ln -f $fn "$PG_EXTENSIONS_DIR/$ln")
+        printf "%b" '\x1b[0m'
+    done
+
+    return 0
+}
 
 echo
-echo "Installation was successful"
+create_hard_links || error=$?
+echo
+
+if [[ -z "$error" ]]; then
+    echo "Installation was successful ✅"
+else
+    echo "Installation aborted ❌ [code:$error]"
+fi
+
